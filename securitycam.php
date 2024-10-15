b829cfb789ba4c962d375f675606d1d9ea592b2c
Maarten Vliegenthart <maarten@maximum.nl>
2024-10-15 22:12:12 +0200

    format

--- securitycam.php
+++ securitycam.php
@@ -15,145 +15,145 @@ use Google\Protobuf\Internal\RepeatedField;
 
 class VisionApiProcessor
 {
-	private ImageAnnotatorClient $vision;
-	private array $ignoreLabels;
-	private string $logFile;
-	
-	public function __construct()
+    private ImageAnnotatorClient $vision;
+    private array $ignoreLabels;
+    private string $logFile;
+
+    public function __construct()
     {
-		$this->loadEnvironment();
-		$this->initializeVisionClient();
-		$this->ignoreLabels = ['Table', 'Chair', 'Coffee table', 'Furniture'];
-		$this->logFile = __DIR__ . '/log.txt';
-	}
-	
-	 private function loadEnvironment(): void
+        $this->loadEnvironment();
+        $this->initializeVisionClient();
+        $this->ignoreLabels = ['Table', 'Chair', 'Coffee table', 'Furniture'];
+        $this->logFile = __DIR__ . '/log.txt';
+    }
+
+    private function loadEnvironment(): void
     {
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
     }
-	
-	private function initializeVisionClient(): void
+
+    private function initializeVisionClient(): void
     {
         $keyPath = __DIR__ . '/' . $_ENV['GOOGLE_APPLICATION_CREDENTIALS'];
         $this->vision = new ImageAnnotatorClient(['credentials' => $keyPath]);
     }
-	
-	public function processImage(): void
-    {
-		$lastMessageFile = __DIR__ . '/lastmessage.txt';
-		$lastMessageTime = file_exists($lastMessageFile) ? (int)file_get_contents($lastMessageFile) : 0;
-		$currentTime = time();
 
-		// Check if 10 minutes (600 seconds) have passed since the last notification
-		if (($currentTime - $lastMessageTime) <= 600) {
-			return; // Exit if less than 10 minutes have passed
-		}
-		try {
-			$imageData = $this->fetchImageFromCamera();
-			$objects = $this->detectObjects($imageData);
-			$filteredObjects = $this->filterObjects($objects);
-			$this->logAndOutputResults($filteredObjects);
-			if (!empty($filteredObjects)) {
-				$this->sendNotification($imageData, $filteredObjects);
-				file_put_contents($lastMessageFile, $currentTime);
-			}
-		} catch (Exception $e) {
-            $this->handleError($e);
-		} finally {
-			$this->vision->close();
-		}
-	}
-	
-	 private function fetchImageFromCamera(): bool|string
+    public function processImage(): void
     {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'http://10.0.0.146/ISAPI/Streaming/channels/101/picture');
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
-		curl_setopt($ch, CURLOPT_USERPWD, 'viewer:' . $_ENV['CAM_PASSWORD']);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$imageData = curl_exec($ch);
-		
-		if (curl_errno($ch)) {
+        $lastMessageFile = __DIR__ . '/lastmessage.txt';
+        $lastMessageTime = file_exists($lastMessageFile) ? (int)file_get_contents($lastMessageFile) : 0;
+        $currentTime = time();
+
+        // Check if 10 minutes (600 seconds) have passed since the last notification
+        if (($currentTime - $lastMessageTime) <= 600) {
+            return; // Exit if less than 10 minutes have passed
+        }
+        try {
+            $imageData = $this->fetchImageFromCamera();
+            $objects = $this->detectObjects($imageData);
+            $filteredObjects = $this->filterObjects($objects);
+            $this->logAndOutputResults($filteredObjects);
+            if (!empty($filteredObjects)) {
+                $this->sendNotification($imageData, $filteredObjects);
+                file_put_contents($lastMessageFile, $currentTime);
+            }
+        } catch (Exception $e) {
+            $this->handleError($e);
+        } finally {
+            $this->vision->close();
+        }
+    }
+
+    private function fetchImageFromCamera(): bool|string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://10.0.0.146/ISAPI/Streaming/channels/101/picture');
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+        curl_setopt($ch, CURLOPT_USERPWD, 'viewer:' . $_ENV['CAM_PASSWORD']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $imageData = curl_exec($ch);
+
+        if (curl_errno($ch)) {
             throw new Exception('Failed to fetch image: ' . curl_error($ch));
         }
-        
+
         curl_close($ch);
         return $imageData;
-	}
-	
-	private function detectObjects(string $imageData): RepeatedField 
-    {
-		$image = (new Image())->setContent($imageData);
-		$feature = (new Feature())->setType(Type::OBJECT_LOCALIZATION);
-		$request = (new AnnotateImageRequest())
-			->setImage($image)
-			->setFeatures([$feature]);
+    }
 
-		$response = $this->vision->batchAnnotateImages([$request]);
-		return $response->getResponses()[0]->getLocalizedObjectAnnotations();
-	}
-	
-	private function filterObjects(RepeatedField $objects): array
+    private function detectObjects(string $imageData): RepeatedField
     {
-		$objectsArray = iterator_to_array($objects);
-		
-		return array_filter($objectsArray, function (LocalizedObjectAnnotation $object) {
+        $image = (new Image())->setContent($imageData);
+        $feature = (new Feature())->setType(Type::OBJECT_LOCALIZATION);
+        $request = (new AnnotateImageRequest())
+            ->setImage($image)
+            ->setFeatures([$feature]);
+
+        $response = $this->vision->batchAnnotateImages([$request]);
+        return $response->getResponses()[0]->getLocalizedObjectAnnotations();
+    }
+
+    private function filterObjects(RepeatedField $objects): array
+    {
+        $objectsArray = iterator_to_array($objects);
+
+        return array_filter($objectsArray, function (LocalizedObjectAnnotation $object) {
             return !in_array($object->getName(), $this->ignoreLabels);
         });
-	}
-	
-	private function logAndOutputResults(array $filteredObjects): void
+    }
+
+    private function logAndOutputResults(array $filteredObjects): void
     {
-		$items = PHP_EOL . date('Y-m-d H:i:s') . PHP_EOL;
+        $items = PHP_EOL . date('Y-m-d H:i:s') . PHP_EOL;
         foreach ($filteredObjects as $object) {
             $items .= $object->getName() . ' ' . round($object->getScore() * 100) . '%' . PHP_EOL;
         }
         echo nl2br($items);
         file_put_contents($this->logFile, $items, FILE_APPEND);
-	}
-	
-	private function handleError(Exception $e): void
+    }
+
+    private function handleError(Exception $e): void
     {
         $errorMessage = 'Error: ' . $e->getMessage() . PHP_EOL;
         echo $errorMessage;
         file_put_contents($this->logFile, $errorMessage, FILE_APPEND);
     }
-	
-	private function sendNotification(string $imageData, array $filteredObjects): void
-	{
-		$message = $this->formatObjectsMessage($filteredObjects);
-		$this->pushoverNotification($message, $imageData);
-	}
 
-	private function formatObjectsMessage(array $filteredObjects): string
-	{
-		return implode(', ', array_map(function($obj) {
-			return $obj->getName() . ' (' . round($obj->getScore() * 100) . '%)';
-		}, $filteredObjects));
-	}
+    private function sendNotification(string $imageData, array $filteredObjects): void
+    {
+        $message = $this->formatObjectsMessage($filteredObjects);
+        $this->pushoverNotification($message, $imageData);
+    }
 
-	private function pushoverNotification(string $message, string $imageData): bool
-	{
-		$ch = curl_init();
-		curl_setopt_array($ch, [
-			CURLOPT_URL => "https://api.pushover.net/1/messages.json",
-			CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => [
-				"token" => $_ENV['PUSHOVER_APP_TOKEN'],
-				"user" => $_ENV['PUSHOVER_USER_KEY'],
-				"message" => $message,
-				"attachment" => curl_file_create('data://application/octet-stream;base64,' . base64_encode($imageData), 'image.jpg', 'image/jpeg'),
-			],
-			CURLOPT_RETURNTRANSFER => true,
-		]);
+    private function formatObjectsMessage(array $filteredObjects): string
+    {
+        return implode(', ', array_map(function ($obj) {
+            return $obj->getName() . ' (' . round($obj->getScore() * 100) . '%)';
+        }, $filteredObjects));
+    }
 
-		curl_exec($ch);
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
+    private function pushoverNotification(string $message, string $imageData): bool
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL            => "https://api.pushover.net/1/messages.json",
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => [
+                "token"      => $_ENV['PUSHOVER_APP_TOKEN'],
+                "user"       => $_ENV['PUSHOVER_USER_KEY'],
+                "message"    => $message,
+                "attachment" => curl_file_create('data://application/octet-stream;base64,' . base64_encode($imageData), 'image.jpg', 'image/jpeg'),
+            ],
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
 
-		return $httpCode === 200;
-	}
+        curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode === 200;
+    }
 }
 
 $processor = new VisionApiProcessor();
